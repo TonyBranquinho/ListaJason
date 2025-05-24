7205ebbc9c6b9c6e6fcee493a383341c53cf5b76
Tony <antony.branquinho@gmail.com>
2025-05-24 18:37:55 -0300

    variaçoes no for e foreach

---
+++ ListaJason/Nomes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaJason
+{
+    internal class Nomes
+    {
+        public void Remover(List<int> ListaNumeros, int excluir)
+        {
+            for (int i = ListaNumeros.Count - 1; i >= 0; i++)
+            {
+                if (excluir == ListaNumeros[i])
+                {
+                    ListaNumeros.RemoveAt(i);
+                }
+            }
+        }
+
+        public void Imprimir(List<int> ListaNumeros)
+        {
+            foreach (int i in ListaNumeros)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ ListaJason/Program.cs
@@ -0,0 +1,22 @@
+﻿using ListaJason;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ListaNumeros = new List<int>() { 1, 2, 3, 4, 5 };
+
+            Nomes nomes = new Nomes();
+
+            Console.WriteLine("Digite um numero a ser excluido");
+            int excluir = int.Parse(Console.ReadLine());
+
+            nomes.Remover(ListaNumeros, excluir);
+
+            Console.WriteLine(ListaNumeros);
+        }
+    }
+}
